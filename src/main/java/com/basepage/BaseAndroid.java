aaee624c5390ea5363afc36cb03f03338f12939b
huquemir <huque.mir@gmail.com>
2022-07-30 03:42:04 -0400

    first upload of tests

---
+++ src/main/java/com/basepage/BaseAndroid.java
@@ -0,0 +1,32 @@
+package com.basepage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BaseAndroid {
+	
+	
+	public static AndroidDriver<AndroidElement> desiredCap() throws MalformedURLException {
+		File f = new File("src/test/java");
+		File fs = new File(f, "app-debug.apk");
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability("systemPort", 8201);
+		cap.setCapability("deviceName", "Pixel_3a_API_31_arm64-v8a");
+		//cap.setCapability("udid", "Pixel_3a_API_31_arm64-v8a");
+		AndroidDriver <AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/wd/hub"), cap);
+		return driver;
+		
+		
+	}
+
+}
---
+++ src/main/java/com/launch/BasicsTest.java
@@ -0,0 +1,34 @@
+package com.launch;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.basepage.BaseAndroid;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class BasicsTest extends BaseAndroid {
+
+@Test	
+public void basicTesting() throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = desiredCap();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElementById("com.example.rollnumbers:id/rollButton").click();
+		AndroidElement ele = driver.findElementById("com.example.rollnumbers:id/textView");
+		Assert.assertEquals(ele.getText(), "How Many?");
+		driver.quit();
+		
+//		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+//		driver.findElementById("android:id/checkbox").click();
+//		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+//		driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
+//		driver.findElementsByClassName("android.widget.Button").get(1).click();
+
+	}
+
+}
